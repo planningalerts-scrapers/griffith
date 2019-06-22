0aa20913dacf869a2327ab9c4a61652826eb1a3b
Matthew Landauer <mlandauer@gmail.com>
2019-06-22 10:44:27 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,3 @@
 require "masterview_scraper"
 
-MasterviewScraper.scrape_and_save_period(
-  url: "https://datracking.griffith.nsw.gov.au",
-  period: :last10days,
-  use_api: true,
-  # Has an incomplete certificate chain. See https://www.ssllabs.com/ssltest/analyze.html?d=datracking.griffith.nsw.gov.au
-  disable_ssl_certificate_check: true
-)
+MasterviewScraper.scrape_and_save(:griffith)
